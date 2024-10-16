2c4bb1ecf6501d9592d0b2ed111ee71269499741
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-10-16 22:57:48 +0300

    #21 add resolver for genres

---
+++ BookHub.Models/Books/BookGenreGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace BookHub.Models.Books;
+
+/// <summary>
+/// Группа, обьединяющая похожиее жанры.
+/// </summary>
+/// <remarks>
+/// По ним наверняка можно фильтровать.
+/// </remarks>
+public enum BookGenreGroup
+{
+    EpicGroup,
+
+    LyricalGroup,
+
+    DramaGroup,
+
+    EpicAndLyricalGroup
+}
