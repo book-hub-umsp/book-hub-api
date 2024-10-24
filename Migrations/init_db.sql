c9d9c8e46bef0738523cbb3bc78d09f0fd233e30
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-10-24 12:20:16 +0300

    #22 update migration

--- Migrations/init_db.sql
+++ Migrations/init_db.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE books_db IF NOT EXISTS;
+CREATE DATABASE books_hub IF NOT EXISTS;
 
 CREATE TYPE book_genre as ENUM (
     'epic_novel', 'novel', 'narrativa', 'story', 'essay', 'parable', 'lyrical_poem', 'elegy', 'epistle', 'epigram', 'ode', 'sonnet', 'comedy', 'tradegy', 'drama', 'poem', 'ballad');
 
-CREATE TYPE book book_status as ENUM ('draft', 'published', 'hiden', 'removed');
+CREATE TYPE book_status as ENUM ('draft', 'published', 'hiden', 'removed');
 
 CREATE TABLE authors (
     id bigint GENERATED ALWAYS AS IDENTITY,
@@ -28,4 +28,11 @@ CREATE TABLE key_words (
     id bigint GENERATED ALWAYS AS IDENTITY,
     content text NOT NULL,
     CONSTRAINT pk_key_words PRIMARY KEY (id)
+);
+
+CREATE TABLE keywords_links(
+    key_word_id bigint NOT NULL,
+    book_id bigint NOT NULL,
+    CONSTRAINT fk_keywords_links_key_word_id FOREIGN KEY (key_word_id) REFERENCES key_words(id) ON DELETE CASCADE,
+    CONSTRAINT fk_keywords_links_book_id FOREIGN KEY (book_id) REFERENCES books(id) ON DELETE CASCADE
 );
