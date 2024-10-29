f3998814034887315c5ec01b4b25aa8948a812af
centuriin <centuriin@yandex.ru>
2024-10-29 22:56:23 +0300

    #39 add test config

---
+++ BookHub.API/TestConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookHub.API;
+
+public class TestConfig
+{
+    [Required(ErrorMessage = "Can't be empty or has only whitespaces.")]
+    public required string Content { get; init; }
+}
