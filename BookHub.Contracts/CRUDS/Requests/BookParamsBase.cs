df2e27af521b7c8fdcd0b95159e4737a75bb3ad9
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-10-31 10:26:00 +0300

    #27 remove command type for api requests

--- BookHub.Contracts/CRUDS/Requests/BookParamsBase.cs
+++ BookHub.Contracts/CRUDS/Requests/BookParamsBase.cs
@@ -7,9 +7,6 @@ namespace BookHub.Contracts.CRUDS.Requests;
 /// <summary>
 /// Маркерная базовая модель параметров.
 /// </summary>
-[TypeConverter(typeof(CommandConverter))]
 public abstract class BookParamsBase
 {
-    [JsonProperty("command_type", Required = Required.Always)]
-    public required CommandType CommandType { get; init; }
 }
