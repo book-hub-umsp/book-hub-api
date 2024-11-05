c02b78dda7016e547bb26d04828498ee54f0d1ce
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-11-05 21:37:51 +0300

    #43 repository fixes

--- BookHub.Storage/PostgreSQL/Repositories/BooksGenresRepository.cs
+++ BookHub.Storage/PostgreSQL/Repositories/BooksGenresRepository.cs
@@ -16,13 +16,15 @@ public sealed class BooksGenresRepository : RepositoryBase, IBooksGenresReposito
     {
     }
 
-    public async Task AddNewGenreAsync(BookGenre bookGenre, CancellationToken token)
+    public async Task AddNewGenreAsync(
+        BookGenre bookGenre, 
+        CancellationToken token)
     {
         ArgumentNullException.ThrowIfNull(bookGenre);
 
         var existedBookGenre =
             await Context.Genres.AsNoTracking()
-                .SingleOrDefaultAsync(x => bookGenre.CompareTo(x.Value));
+                .SingleOrDefaultAsync(x => bookGenre.CompareTo(x.Value), token);
 
         if (existedBookGenre is not null)
         {
@@ -33,20 +35,23 @@ public sealed class BooksGenresRepository : RepositoryBase, IBooksGenresReposito
         _ = Context.Genres.Add(new() { Value = bookGenre.Value });
     }
 
-    public async Task<IReadOnlyCollection<BookGenre>> GetAllGenresAsync(CancellationToken token)
+    public async Task<IReadOnlyCollection<BookGenre>> GetAllGenresAsync(
+        CancellationToken token)
     {
-        var storageGenres = await Context.Genres.ToListAsync(token);
+        var storageGenres = await Context.Genres.AsNoTracking().ToListAsync(token);
 
         return storageGenres.Select(x => new BookGenre(x.Value)).ToList();
     }
 
-    public async Task RemoveGenreAsync(BookGenre bookGenre, CancellationToken cancellationToken)
+    public async Task RemoveGenreAsync(
+        BookGenre bookGenre, 
+        CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(bookGenre);
 
         var existedBookGenre =
             await Context.Genres.AsNoTracking()
-                .SingleOrDefaultAsync(x => bookGenre.CompareTo(x.Value))
+                .SingleOrDefaultAsync(x => bookGenre.CompareTo(x.Value), cancellationToken)
                 ?? throw new InvalidOperationException(
                     $"Book genre '{bookGenre.Value}' is not already exists.");
 
