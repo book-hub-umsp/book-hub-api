e505d18bdc1976d049a7a2cc413d0d13abb9a10f
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-11-05 12:49:08 +0300

    #43 add genres repository

---
+++ BookHub.Abstractions/Storage/Repositories/IBooksGenresRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookHub.Models.Books;
+
+namespace BookHub.Abstractions.Storage.Repositories;
+
+/// <summary>
+/// Описывает интерфейс для работы с жанрами книг.
+/// </summary>
+public interface IBooksGenresRepository
+{
+    public Task AddNewGenreAsync(BookGenre bookGenre, CancellationToken token);
+
+    public Task<IReadOnlyCollection<BookGenre>> GetAllGenresAsync(CancellationToken token);
+
+    public Task RemoveGenreAsync(BookGenre bookGenre, CancellationToken cancellationToken);
+}
---
+++ BookHub.Storage/PostgreSQL/Repositories/BooksGenresRepository.cs
@@ -0,0 +1,55 @@
+﻿using BookHub.Abstractions.Storage.Repositories;
+using BookHub.Models.Books;
+using BookHub.Storage.PostgreSQL.Abstractions;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHub.Storage.PostgreSQL.Repositories;
+
+/// <summary>
+/// Представляет репозиторий жанров книг.
+/// </summary>
+public sealed class BooksGenresRepository : RepositoryBase, IBooksGenresRepository
+{
+    public BooksGenresRepository(IRepositoryContext context) 
+        : base(context)
+    {
+    }
+
+    public async Task AddNewGenreAsync(BookGenre bookGenre, CancellationToken token)
+    {
+        ArgumentNullException.ThrowIfNull(bookGenre);
+
+        var existedBookGenre =
+            await Context.Genres.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Value == bookGenre.Value);
+
+        if (existedBookGenre is not null)
+        {
+            throw new InvalidOperationException(
+                $"Book genre '{bookGenre.Value}' is already exists.");
+        }
+
+        _ = Context.Genres.Add(new() { Value = bookGenre.Value });
+    }
+
+    public async Task<IReadOnlyCollection<BookGenre>> GetAllGenresAsync(CancellationToken token)
+    {
+        var storageGenres = await Context.Genres.ToListAsync(token);
+
+        return storageGenres.Select(x => new BookGenre(x.Value)).ToList();
+    }
+
+    public async Task RemoveGenreAsync(BookGenre bookGenre, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(bookGenre);
+
+        var existedBookGenre =
+            await Context.Genres.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.Value == bookGenre.Value)
+                ?? throw new InvalidOperationException(
+                    $"Book genre '{bookGenre.Value}' is not already exists.");
+
+        _ = Context.Genres.Remove(existedBookGenre);
+    }
+}
