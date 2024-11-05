0acb74c19e8ffb2022c02a8039b42cc42ccd054a
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-11-05 13:33:26 +0300

    #43 add dtos

---
+++ BookHub.Contracts/REST/Responces/BooksGenresListResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Newtonsoft.Json;
+
+namespace BookHub.Contracts.REST.Responces;
+
+/// <summary>
+/// Ответ на запрос о получении всех жанров.
+/// </summary>
+public sealed class BooksGenresListResponse
+{
+    [Required]
+    [JsonProperty("books_genres", Required = Required.Always)]
+    public required IReadOnlyCollection<string> BookGenres { get; init; }
+}
