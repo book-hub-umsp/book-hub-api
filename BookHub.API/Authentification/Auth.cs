72bd06dabadb667630e4eb78cfbd074b6debf112
centuriin <centuriin@yandex.ru>
2024-11-10 14:05:49 +0300

    #65 change default policy

--- BookHub.API/Authentification/Auth.cs
+++ BookHub.API/Authentification/Auth.cs
@@ -1,7 +1,13 @@
 ﻿namespace BookHub.API.Authentification;
 
+/// <summary>
+/// Содержит константы для описания аутентификации/авторизации.
+/// </summary>
 public struct Auth
 {
+    /// <summary>
+    /// Содержит константы провайдеров аутентификации.
+    /// </summary>
     public struct AuthProviders
     {
         public const string GOOGLE = "google";
@@ -9,6 +15,9 @@ public struct Auth
         public const string YANDEX = "yandex";
     }
 
+    /// <summary>
+    /// Содержит константы типов специфических клеймов.
+    /// </summary>
     public struct ClaimTypes
     {
         public const string USER_ID_CLAIM_NAME = "user_id";
