8b8c4b3fa9ec2b08dcac9fb03d74b3df3711fdc8
centuriin <centuriin@yandex.ru>
2024-11-14 00:30:58 +0300

    #71 add domain pagination models

---
+++ BookHub.Models/API/Pagination/PagePagging.cs
@@ -0,0 +1,12 @@
+﻿namespace BookHub.Models.API.Pagination;
+
+/// <summary>
+/// Модель запроса на пагинацию для <see cref="PagePagination"/>.
+/// </summary>
+/// <param name="Page">
+/// Запрашиваемая страница.
+/// </param>
+/// <param name="PageSize">
+/// Размер страницы.
+/// </param>
+public sealed record class PagePagging(int Page, int PageSize);
