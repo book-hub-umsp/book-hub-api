7b0f944d198dbfe77de353daa685232fdf39f846
Kirill Parakhin <kirill.parakhin@altenar.com>
2024-11-16 20:52:47 +0300

    #66 fix roles migration

--- migrations/001_create_user.up.sql
+++ migrations/001_create_user.up.sql
@@ -3,6 +3,13 @@ BEGIN;
 CREATE TYPE user_status as ENUM ('active', 'blocked', 'deleted');
 CREATE TYPE claim_type as ENUM ('none', 'moderate_comments', 'moderate_reviews', 'create_topics', 'change_book_visibility', 'manage_user_actions', 'manage_user_accounts', 'change_user_role', 'add_new_role', 'change_role_claims');
 
+CREATE TABLE roles (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    claims claim_type[] NOT NULL DEFAULT '{"none"}',
+    CONSTRAINT "pk_roles" PRIMARY KEY (id)
+);
+
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     role_id BIGINT NOT NULL DEFAULT 1,
@@ -14,12 +21,6 @@ CREATE TABLE users (
         "pk_users" PRIMARY KEY (id),
     CONSTRAINT "fk_users_role_id" 
         FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET DEFAULT
-);
-
-CREATE TABLE roles (
-    id BIGINT GENERATED ALWAYS AS IDENTITY,
-    name TEXT NOT NULL,
-    claims claim_type[] NOT NULL DEFAULT '{"none"}'
 );
 
 INSERT INTO roles (name, claims) VALUES ('default_user', '{"none"}');
